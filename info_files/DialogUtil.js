e08620372a3b929c23c531f36b8e7d2d7c656ac7
eric895 <113846898+eric895@users.noreply.github.com>
2022-09-19 03:50:04 +0800

    Add files via upload

---
+++ info_files/DialogUtil.js
@@ -0,0 +1,16 @@
+/**
+ * 为了ligerUi的顶层dialog显示
+ */
+var DialogUtil = {
+	open : function(opts){
+		if(opts.type=="post"){
+			opts.postUrl = opts.url;
+			opts.url=__ctx+"/platform/form/post/dialog.ht";
+		}
+		var outerWindow =window.top;
+//		return outerWindow.$.ligerDialog.open(opts);
+//		return $.ligerDialog.open(opts);
+		return parent.$.ligerDialog.open(opts);
+		
+	}
+}
---
+++ info_files/dynamic.jsp
@@ -0,0 +1,10 @@
+
+
+
+var pathName = window.document.location.pathname;
+var __ctx = pathName.substring(0,pathName.substring(1).indexOf('/')+1);
+if(__ctx == '/platform'){
+	__ctx='';
+}
+var ctx=__ctx;
+var __jsessionId='E93687E03D007F0DA791007C82D96407';
